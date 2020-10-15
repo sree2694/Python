c8111bbd7f2cf68fcfee43a38907703507f1078a
sree2694 <58017164+sree2694@users.noreply.github.com>
2020-10-15 12:06:20 +0530

    calcarea

---
+++ Basics/areacals.py
@@ -0,0 +1,8 @@
+# name = input("Tell me your name,Raj : ")
+# age = input('Your age : ')
+# print('hello '+name,'you are',age)
+
+# Calc area of the circle
+radius = input("Enter the radius of the Circle(m) : ")
+area = 3.142 *int(radius)**2
+print("The area of your circle is : ", area)
